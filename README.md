a6bbfa4b3b716fdc6aad97177a660abcd73a8cae
Julio Cantu <julio.c.cantu03@utrgv.edu>
2020-03-07 14:44:14 -0600

    Add kittens controller and model

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# README
+# Odin-Kittens
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+This project is a short one assigned at https://www.theodinproject.com/courses/ruby-on-rails/lessons/apis
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+with the intent of practicing the creation of a simple internal api in a rails application.
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,54 @@
+class KittensController < ApplicationController
+    def index
+        @kittens = Kitten.all
+    end
+    
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params)
+
+        if @kitten.save
+            flash[:notice] = "Kitten successfully created!"
+            redirect_to @kitten
+        else
+            flash[:alert] = "An error occurred."
+            render 'new'
+        end
+    end
+
+    def show
+        @kitten = Kitten.find_by(:id => params[:id])
+    end
+
+    def edit
+        @kitten = Kitten.find_by(:id => params[:id])
+    end
+
+    def update
+        @kitten = Kitten.find_by(:id => params[:id])
+
+        if @kitten.update(kitten_params)
+            flash[:notice] = "Kitten successfully updated!"
+            redirect_to @kitten
+        else
+            flash[:alert] = "An error occurred."
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @kitten = Kitten.find_by(:id => params[:id])
+        @kitten.destroy
+        flash[:notice] = "Kitten successfully deleted!"
+        redirect_to root_path
+    end
+
+    private
+
+      def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+      end
+end
